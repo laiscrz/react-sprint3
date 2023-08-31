dc72044ff2215cb3fd29a1ec3b738385ec39ebe0
Lais <lais.albino2003@gmail.com>
2023-08-31 14:34:45 -0300

    add pasta components e começando mexer no cabeçalho e roda pé

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+
+export default function Cabecalho(){
+    return(
+        <div className="cabecalho-conteiner">
+            <header>
+
+            </header>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/comofunciona'>Como Funciona</Link>
+                <Link to='/servicos'>Serviços</Link>
+                <Link to='/sobrenos'>Sobre Nós</Link>
+                <Link to='/contato'>Contato</Link>
+            </nav>
+        </div>
+    )
+}
---
+++ src/components/Rodape.jsx
@@ -0,0 +1,8 @@
+
+export default function Rodape(){
+    return(
+        <footer class='footer-conteiner'>
+        <p>&copy; 2023 Inova-X. Todos os direitos reservados.</p>
+    </footer> 
+    )
+}

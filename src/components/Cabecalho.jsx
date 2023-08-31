90d54dac1407d38e10f9f023a5eaa439d4f631c8
lucasdslopes <127765306+lucasdslopes@users.noreply.github.com>
2023-08-31 16:05:42 -0300

    Colocando button de login

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -12,6 +12,7 @@ export default function Cabecalho(){
                 <Link to='/servicos'>Serviços</Link>
                 <Link to='/sobrenos'>Sobre Nós</Link>
                 <Link to='/contato'>Contato</Link>
+                <button>login<button/>
             </nav>
         </div>
     )

ec99992deb26dfa9ad609dd203110523109979d8
Lais <lais.albino2003@gmail.com>
2023-08-31 21:21:21 -0300

    alteração de foto profilr_icon e algumas mudanças no cabeçalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,18 +1,28 @@
 import {Link} from 'react-router-dom'
+import profile_icon from '../image/profile_icon.png';
 
 export default function Cabecalho(){
     return(
         <div className="cabecalho-conteiner">
             <header>
-
+                <img src="src/image/logoInovax.png" alt="img" className='logo'/>
             </header>
             <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/comofunciona'>Como Funciona</Link>
-                <Link to='/servicos'>Serviços</Link>
-                <Link to='/sobrenos'>Sobre Nós</Link>
-                <Link to='/contato'>Contato</Link>
+                <div className='navegacao-links'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/comofunciona'>Como Funciona</Link>
+                    <Link to='/servicos'>Serviços</Link>
+                    <Link to='/sobrenos'>Sobre Nós</Link>
+                    <Link to='/contato'>Contato</Link>
+                </div>
             </nav>
+                <div>
+                    <Link to='/login'>
+                        <button id="btnLogin">
+                            <span><img src={profile_icon} alt="Profile Icon"/> Login</span>
+                        </button>
+                    </Link> 
+                </div>    
         </div>
     )
 }

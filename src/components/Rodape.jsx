83b98178320d18b7928e5f067ad0a279eacdb2a7
Lais <lais.albino2003@gmail.com>
2023-09-02 11:53:32 -0300

    add imagens de icone de rede socias no footer

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,8 +1,13 @@
 
-export default function Rodape(){
-    return(
-        <footer class='footer-conteiner'>
-        <p>&copy; 2023 Inova-X. Todos os direitos reservados.</p>
-    </footer> 
-    )
-}
+export default function Rodape() {
+    return (
+      <footer className='footer-conteiner'>
+        <section className="redes-sociais">
+          <a href="https://pt-br.facebook.com/"><img src="src/image/icone_face.jpg" alt="Facebook Icone" className="icone-redes" /></a>
+          <a href="https://www.instagram.com/"><img src="src/image/icone_insta.jpg" alt="Instagram Icone" className="icone-redes" /></a>
+          <a href="https://web.whatsapp.com/"><img src="src/image/icone_zap.jpg" alt="Whatsapp Icone" className="icone-redes" /></a>
+        </section>
+        <p className="text-center">&copy; 2023 Inova-X. Todos os direitos reservados.</p>
+      </footer>
+    );
+  }

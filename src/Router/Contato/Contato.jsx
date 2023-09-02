b47cd15c925e4f56edbea4d0eabc6ba2956cfdf6
Lais <lais.albino2003@gmail.com>
2023-09-02 16:49:33 -0300

    alterando nome da pasta Sobre Nos, e add conteudo nela e na Pagina contatos

--- src/Router/Contato/Contato.jsx
+++ src/Router/Contato/Contato.jsx
@@ -1,8 +1,105 @@
+import React, { useState } from 'react';
 
-export default function Contato(){
-    return(
-        <main>
-            
-        </main>
-    )
+export default function Contato() {
+  const [formData, setFormData] = useState({
+    nome: '',
+    telefone: '',
+    email: '',
+    assunto: '',
+    comentarios: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Verifique se os campos obrigatórios estão preenchidos
+    if (
+      formData.nome === '' ||
+      formData.telefone === '' ||
+      formData.email === '' ||
+      formData.assunto === ''
+    ) {
+      window.alert('Por favor, preencha todos os campos obrigatórios.'); // Mostra um alerta
+      return; // Sai da função sem enviar o formulário
+    }
+
+    console.log('Dados enviados:', formData);
+
+    // Você pode adicionar aqui o código para enviar os dados para o servidor, se necessário.
+  };
+
+  return (
+    <main>
+      <div className="contato-conteiner">
+        <h1>Contato</h1>
+        <h3>Fale Conosco</h3>
+        <p>
+          Para entrar em contato conosco, por favor, preencha o formulário
+          abaixo com suas informações e a sua mensagem. Nós responderemos o mais
+          rápido possível. Também é possível enviar um e-mail para nosso
+          endereço de contato ou entrar em contato através de nossas redes
+          sociais. Estamos sempre abertos a sugestões, comentários e críticas
+          construtivas para melhorar nossos produtos e serviços. Obrigado por
+          escolher a InovaX.
+        </p>
+        <h3>Formulário para Contato</h3>
+        <div className="form-contato">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="nome"
+              placeholder="Nome Completo"
+              value={formData.nome}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              name="telefone"
+              placeholder="Telefone"
+              value={formData.telefone}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <select
+              name="assunto"
+              value={formData.assunto}
+              onChange={handleChange}
+            >
+              <option value="" disabled>
+                Assunto
+              </option>
+              <option value="elogio">Elogio</option>
+              <option value="duvida">Dúvidas</option>
+              <option value="reclamacoes">Reclamações</option>
+              <option value="outros">Outros</option>
+            </select>
+            <textarea
+              name="comentarios"
+              placeholder="Comentários"
+              value={formData.comentarios}
+              onChange={handleChange}
+            ></textarea>
+            <br />
+            <button type="submit" id="btnEnviar">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/Router/SobreNos/Sobrenos.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const sections = [
+  {
+    title: 'Sobre a nossa empresa',
+    content: 'A InovaX é uma empresa brasileira de tecnologia, focada no desenvolvimento de soluções inovadoras para diversos segmentos de mercado. Fundada em 2023, a empresa nasceu da paixão de um grupo de empreendedores por inovação e tecnologia, com o objetivo de ajudar a transformar o mundo em um lugar melhor por meio da tecnologia.  Acreditamos que a tecnologia pode transformar vidas e nosso principal produto, a solução digital de vistoria de bicicletas, reflete essa visão. Contamos com uma equipe experiente e comprometida em fornecer soluções de alta qualidade e fazer a diferença na vida das pessoas.',
+  },
+  {
+    title: 'Nossa Missão',
+    content: 'Nossa missão na InovaX é utilizar a tecnologia para criar soluções inovadoras que não só facilitem a vida das pessoas, mas também promovam um impacto positivo na sociedade e no meio ambiente. Buscamos constantemente a excelência em nossos produtos e serviços, com o objetivo de melhorar a qualidade de vida das pessoas e transformar o mundo em um lugar melhor.',
+  },
+  {
+    title: 'Nossos Valores',
+    content: 'Acreditamos que nossos valores são fundamentais para o sucesso da nossa empresa e de nossos clientes:',
+    list: [
+      'Inovação: buscamos sempre soluções criativas e inovadoras para os problemas dos nossos clientes;',
+      'Ética: agimos sempre com ética, transparência e respeito;',
+      'Qualidade: entregamos sempre soluções de alta qualidade e confiabilidade;',
+      'Satisfação do cliente: trabalhamos com foco na satisfação dos nossos clientes, buscando entender e atender suas necessidades de forma personalizada;',
+      'Trabalho em equipe: valorizamos e incentivamos o trabalho em equipe, a colaboração e a diversidade de ideias.',
+    ],
+  },
+  {
+    title: 'Outros detalhes relevantes',
+    list: [
+      'Localização: São Paulo, Brasil;',
+      'Ano de fundação: 2023',
+      'Setores de atuação: tecnologia, inovação e desenvolvimento de software;',
+      'Clientes: diversos segmentos de mercado, incluindo financeiro, varejo e seguros;',
+      'Parceiros: mantemos parcerias estratégicas com empresas e instituições de renome, para garantir a qualidade e a inovação de nossas soluções.',
+    ],
+  },
+];
+
+export default function SobreNos() {
+  return (
+    <main>
+      <div className="sobrenos-conteiner">
+        <h1>Sobre Nós</h1>
+        {sections.map((section, index) => (
+          <div key={index}>
+            <h3>{section.title}</h3>
+            <p>{section.content}</p>
+            {section.list && (
+              <ul>
+                {section.list.map((item, itemIndex) => (
+                  <li key={itemIndex}>{item}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}

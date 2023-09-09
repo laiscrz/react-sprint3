70e9c835944b8665f608528d2d50c1cfad4407b2
Lais <lais.albino2003@gmail.com>
2023-09-09 09:31:09 -0300

    icons add em pasta image, estilização em portal cliente, e atualizações em pagina de Login e em suas estilizações

--- src/Router/Login/Login.jsx
+++ src/Router/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '/src/Styles/Login.css'
 
+
 export default function Login() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
@@ -15,37 +16,58 @@ export default function Login() {
 
   return (
     <main>
-      <div className="form-login">
-      <form onSubmit={handleSubmit}>
-        <div className='title-login'>
-          <h2>Faça o seu login</h2>
-        </div>
-        <div className='caixa-login'>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder="Email"
-          />
-        </div>
-        <div className='caixa-login'>
-          <input
-            type="password"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            required
-            placeholder="Senha"
-          />
-        </div>
-        <button type="submit">Entrar</button>
-        <div className='cadastro-link'>
-          <p>Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link></p>
-        </div>
-      </form>
-      </div>
+      <div className="container">
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form" onSubmit={handleSubmit}>
+              <span className="login-form-title">
+                Faça o seu login
+              </span><br />
 
+              <div className="wrap-input margin-bottom-35">
+                <input
+                  className="input-form"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <span className="focus-input-form" data-placeholder="E-mail"></span>
+              </div>
+
+              <div className="wrap-input margin-bottom-35">
+                <input
+                  className="input-form"
+                  type="password"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                />
+                <span className="focus-input-form" data-placeholder="Senha"></span>
+              </div>
+
+              <div className="container-login-form-btn">
+                <button className="login-form-btn" type="submit">
+                  Entrar
+                </button>
+              </div>
+
+              <ul className="login-utils">
+                <li className="margin-bottom-8 margin-top-8">
+                  <span className="text1">
+                    Não tem uma conta? 
+                  </span>
+
+                  <Link to="/cadastro" className="text2">
+                    Cadastre-se
+                  </Link>
+                </li>
+              </ul>
+            </form>
+          </div>
+          <img src="src/image/login.png" width="300" height="300" class="margin-left-50" />
+        </div>
+      </div>
     </main>
   )
 }
-

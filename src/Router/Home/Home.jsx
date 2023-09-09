764a6fe56931cd57b14f53fc12b4714b517da418
Lais <lais.albino2003@gmail.com>
2023-09-09 11:13:36 -0300

    add conteudo inicial em home e estilizando, e pequenas alterações em app.css

--- src/Router/Home/Home.jsx
+++ src/Router/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import '/src/Styles/Home.css';
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <main>
+            <div className="home-conteiner">
+                <section className='inicio-home'>
+                    <div className="destaq-inicio">
+                        <div className="frase-destaq">
+                        <h1>Sua Vistoria</h1>
+                        <h2>Online Aqui!</h2>
+                        </div>
+                        <div className="btn-saibamais">
+                            <button>Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div className="destaq-bicle">
+                        <div className="quadrad-home">
 
+                        </div>
+                        <img src="src/image/img_homedestaq.png" alt="Homem na Bicicleta" />
+                    </div>
+                </section>
+            </div>
         </main>
     )
 }

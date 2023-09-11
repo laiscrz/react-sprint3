44dcbbe6fa1f2ebc5b957a89d32c97a72fff763f
Lais <lais.albino2003@gmail.com>
2023-09-11 16:59:02 -0300

    add link do repositorio do github

--- src/Router/Integrantes/Integrantes.jsx
+++ src/Router/Integrantes/Integrantes.jsx
@@ -11,6 +11,9 @@ export default function Integrantes() {
                     <p> Lucas do Santos Lopes - RM 550790 </p>
                     <p> Murilo Machado - RM 550718</p>
                     <p> Victor Taborda - RM 97900</p>
+
+                    <h1>Link do Repositório Git/GitHub</h1>
+                    <p>Lembrando, que o repositório no GitHub é uma adição ao projeto e não é um requisito obrigatório para esta documentação. Nosso professor informou que não vai cobrar a inclusão do GitHub neste trabalho, mas decidimos compartilhar o link do nosso repositório como uma referência opcional. Você pode acessar o repositório <a href="https://github.com/laiscrz/react-sprint3">clicando aqui</a>.</p>
                 </section>
             </div>
         </main>

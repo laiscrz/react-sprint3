160cf7814ce319f9bad1c90a7aa1b6139009d85e
Lais <lais.albino2003@gmail.com>
2023-09-11 17:15:20 -0300

    alteração de nome do integrante

--- src/Router/Integrantes/Integrantes.jsx
+++ src/Router/Integrantes/Integrantes.jsx
@@ -8,7 +8,7 @@ export default function Integrantes() {
                     <h1>Integrantes</h1>
                     <p> Gustavo Marques Catelan - RM 551823 </p>
                     <p> Lais Alves da Silva Cruz - RM 552258</p>
-                    <p> Lucas do Santos Lopes - RM 550790 </p>
+                    <p> Lucas dos Santos Lopes - RM 550790 </p>
                     <p> Murilo Machado - RM 550718</p>
                     <p> Victor Taborda - RM 97900</p>
 
